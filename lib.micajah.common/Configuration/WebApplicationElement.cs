fc2d5c3e3a2ce51a602a2105ae5a0b30c526304e
yurayuriev <yura.yuriev@gmail.com>
2012-11-26 15:33:57 +0400

    15 183   	API key in mc3

--- lib.micajah.common/Configuration/WebApplicationElement.cs
+++ lib.micajah.common/Configuration/WebApplicationElement.cs
@@ -18,7 +18,7 @@ namespace Micajah.Common.Configuration
         /// <summary>
         /// The required schema version of the database.
         /// </summary>
-        internal const int RequiredDatabaseVersion = 103;
+        internal const int RequiredDatabaseVersion = 104;
 
         private string m_ApplicationUrl;
         private object m_AuthenticationMode;
